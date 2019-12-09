04c12a03509850d2730fe307f2cbe2d52804a4df
Asif Aziz <asifaziz01@users.noreply.github.com>
2019-12-09 09:45:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,14 @@ import ProductGrid from '~/components/ProductGrid'
 const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <section>
     <h1>Hi people</h1>
     <p>Welcome to your new Shop powered by Gatsby and Shopify. First Shopify</p>
+    </section>
+    <section>
     <ProductGrid />
+    </section>
+  
     <Link to="/page-2/">Go to page 2</Link>
   </>
 )
