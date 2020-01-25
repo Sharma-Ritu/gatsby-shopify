2828165a0e24d08c7072562935fcfa70dd548553
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-01-25 19:42:55 +0530

    I updated index.js file in Cart folder.
    I updated index.js file in Cart folder.

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-
+import {Container,Jumbotron, Row, Col, Button, Media, Table} from 'reactstrap';
 import StoreContext from '~/context/StoreContext'
 import LineItem from './LineItem'
 
@@ -17,19 +17,24 @@ const Cart = () => {
   })
 
   return (
-    <div>
-      {line_items}
-      <h2>Subtotal</h2>
-      <p>$ {checkout.subtotalPrice}</p>
-      <br />
-      <h2>Taxes</h2>
-      <p>$ {checkout.totalTax}</p>
-      <br />
-      <h2>Total</h2>
-      <p>$ {checkout.totalPrice}</p>
-      <br />
-      <button onClick={handleCheckout}>Check out</button>
-    </div>
+    <>
+    <section>
+      <div className="container-large">
+        <Row>
+          
+          <ul className="pl-0 list-unstyled w-100 cart-total">
+              <li className="d-flex w-100 no-gutters"><strong>CART TOTAL</strong></li>
+              <li className="d-flex w-100 no-gutters"><div className="col-7">SUBTOTAL</div><div className="col-5">CAD$ {checkout.subtotalPrice}</div></li>
+              <li className="d-flex w-100 no-gutters"><div className="col-7">SHIPPING</div><div className="col-5">Enter your address to view shipping options.</div></li>
+              <li className="d-flex w-100 no-gutters"><div className="col-7">TOTAL</div><div className="col-5 color-primary font-weight-bold">CAD$ {checkout.totalPrice}</div></li>
+          </ul>
+          <div className="col-12 pt-5 checkout">
+            <button className="btn p-2 space-1 float-right font-weight-bold btn-checkout color-primary"> PROCEED TO CHECKOUT </button>
+          </div>
+        </Row>
+      </div>
+    </section>
+    </>
   )
 }
 
