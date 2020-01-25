aa3d3cb75738f911897607eab2188c3a7f2b2d03
Asif Aziz <d_asifaziz01@yahoo.com>
2020-01-25 16:56:15 +0530

    Signed-off-by: Asif Aziz <d_asifaziz01@yahoo.com>

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -6,14 +6,14 @@ import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
 import run from "../assets/img/run.png"
 
-const Blog = ({ data }) =>  (
-    <Container>
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
       <div>Hello world</div>
-	  console.log(data)
-    </Container>
-  );
-
-
+    </Layout>
+  )
+}
 export const query = graphql`
   query {
     allFile {
