bb1a70c645ab5a6bfbba0a93b97de5083a83f39c
Asif Aziz <asifaziz01@users.noreply.github.com>
2020-01-25 20:50:56 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -30,7 +30,7 @@ const Cart = () => {
               <li className="d-flex w-100 no-gutters"><div className="col-7">TOTAL</div><div className="col-5 color-primary font-weight-bold">CAD$ {checkout.totalPrice}</div></li>
           </ul>
           <div className="col-12 pt-5 checkout">
-            <button className="btn p-2 space-1 float-right font-weight-bold btn-checkout color-primary"> PROCEED TO CHECKOUT </button>
+           <button className="btn p-2 space-1 float-right font-weight-bold btn-checkout color-primary onClick={handleCheckout} disabled={checkout.lineItems.length === 0}">
           </div>
         </Row>
       </div>
