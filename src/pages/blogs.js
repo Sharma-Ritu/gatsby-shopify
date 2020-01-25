efc6ccd401dbb6264a1d901ca1c36f6c6e1a006a
Asif Aziz <d_asifaziz01@yahoo.com>
2020-01-25 14:56:56 +0530

    Sttarting Blog Page
    Signed-off-by: Asif Aziz <d_asifaziz01@yahoo.com>

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,74 +1,31 @@
 import React from 'react';
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
 import run from "../assets/img/run.png"
 
-
-export default (props) => {
+export default ({ data }) => {
+  console.log(data)
   return (
-	<>
-		<Header />
-		<section>
-			<Jumbotron className="mb-0 text-center color-primary">
-				<h1 className="font-weight-bold display-4 erbaum-bold">Coming Soon...</h1>
-			</Jumbotron>
-		</section>
-		<section className="mb-0 py-5 approved bg-image">
-			<Container>
-				<Row>
-					<Col sm="4" className="align-middle">
-						<img src={run} alt="run" width="70%"/>
-					</Col>
-					<Col sm="8">
-						<div className="text-white mb-4 mt-4 pt-3 pl-3">
-							<h4 className="lead-text-font text-white erbaum-bold display-5">CUSTOMER APPROVED</h4>
-							<p className="proxima-b space-1col-12 col-sm-7 p-0" style={{fontSize:'14px'}}>Our customers have reported the following benefits after sleeping on there chirofoam mattress</p>
-							<div className="pt-3">
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									97%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improvement in sleep comfort and quality
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									94%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Increased energy in the morning and throughout the day
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									86%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improved muscle recovery and/or relief in daliy experienced back pain
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									82%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improvement in sleep comfort and quality
-								  </Media>
-								</Media>
-								<p className="cta erbaum mt-3 mt-sm-0">
-									<a href="#" className="text-white">READ CUSTOMER REVIEW <i class="fa fa-caret-right ic1"></i><i class="fa fa-caret-right ic2"></i><i class="fa fa-caret-right ic3"></i></a>
-								</p>
-							</div>
-						</div>
-					</Col>
-				</Row>
-			</Container>
-		</section>
+    <Container>
+      <div>Hello world</div>
+    </Container>
+  )
+}
 
-		<Footer />
-	</>
-  );
-};
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
