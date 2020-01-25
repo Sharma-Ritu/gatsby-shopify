dc16ae9ebf60380427d906cf7c7ec96ad01e96d4
Asif Aziz <d_asifaziz@yahoo.com>
2020-01-25 19:50:59 +0530

    Signed-off-by: Asif Aziz <d_asifaziz01@yahoo.com>

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { graphql } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import {Container, Row, Col, Button, Media} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
 import run from "../assets/img/run.png"
 
 const ProductsPage = ({ data }) => (
   <Container>
     <h1>Products</h1>
-    <ul>
+    <Row>
       {data.allShopifyProduct.edges.map(({ node }) => (
-        <li key={node.shopifyId}>
+        <div key={node.shopifyId}>
           <h3>
-            <Link to={`/product/${node.handle}`}>{node.title}</Link>
+            {node.title}
             {" - "}${node.priceRange.minVariantPrice.amount}
           </h3>
           <p>{node.description}</p>
-        </li>
+        </div>
       ))}
-    </ul>
+    </Row>
   </Container>
 )
 
