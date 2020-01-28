92a910e239a667faa964abcabc14f4ec6ebeb588
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-01-28 20:24:06 +0530

    Update blogs.js

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -4,6 +4,7 @@ import Footer from "../components/footer"
 import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
 import blogs1 from "../assets/img/blogs5.jpg"
+import Blogs from "../components/Blogs"
 
 
 export default (props) => {
@@ -19,89 +20,7 @@ export default (props) => {
 		<section className="mb-0 py-5 position-relative">
 			<div className="container-large">
 				<Row>
-					<Col sm="8" className="align-middle">
-						<div className="blogs-section mb-5">
-							<div className="featured-image position-relative overflow-hidden">
-								<a href="#">
-									<img src={blogs1} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out'}}/>
-									<div className="card-layer">
-										<div className="img-icon position-absolute" style={{top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>
-											<i class="fa fa-image" style={{fontSize:'50px', color:'#fff'}}></i>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div className="blogs-content d-flex mt-4 pb-4">
-								<Col sm="1" className="blog-icon text-center border-right pr-0 d-table">
-									<p style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-share-alt"></i><span className="d-block">2</span></p>
-									<p className="border-top border-bottom py-2" style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-envelope"></i><span className="d-block">2</span></p>
-									<p className="mb-0" style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-heart"></i><span className="d-block">2</span></p>
-								</Col>
-								<Col sm="11" className="pl-4">
-									<p style={{fontSize:'12px'}}>By <span>Chirofoam</span> In <span>Sleep</span> Posted <span>June 15, 2019</span></p>
-									<h3 className="mb-3"><a href="" className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>4 Benefits of Juicing and How it Helps Improve Sleep</a></h3>
-									<p className="filson-pro-reg" style={{fontSize:'14px'}}>Sleep; the blissful seven to eight hours we spend every night resting cut off from the world sets the tone for the next day. A good rest is a must for having a productive day as sleep helps you [...]</p>
-									<p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
-										<a href="/single-blog/" className="btn-cta color-primary erbaum-bold space-1">READ MORE</a>
-									</p>
-								</Col>
-							</div>
-						</div>
-						<div className="blogs-section mb-5">
-							<div className="featured-image position-relative overflow-hidden">
-								<a href="#">
-									<img src={blogs1} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out'}}/>
-									<div className="card-layer">
-										<div className="img-icon position-absolute" style={{top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>
-											<i class="fa fa-image" style={{fontSize:'50px', color:'#fff'}}></i>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div className="blogs-content d-flex mt-4 pb-4">
-								<Col sm="1" className="blog-icon text-center border-right pr-0 d-table">
-									<p style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-share-alt"></i><span className="d-block">2</span></p>
-									<p className="border-top border-bottom py-2" style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-envelope"></i><span className="d-block">2</span></p>
-									<p className="mb-0" style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-heart"></i><span className="d-block">2</span></p>
-								</Col>
-								<Col sm="11" className="pl-4">
-									<p style={{fontSize:'12px'}}>By <span>Chirofoam</span> In <span>Sleep</span> Posted <span>June 15, 2019</span></p>
-									<h3 className="mb-3"><a href="" className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>4 Benefits of Juicing and How it Helps Improve Sleep</a></h3>
-									<p className="filson-pro-reg" style={{fontSize:'14px'}}>Sleep; the blissful seven to eight hours we spend every night resting cut off from the world sets the tone for the next day. A good rest is a must for having a productive day as sleep helps you [...]</p>
-									<p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
-										<a href="/single-blog/" className="btn-cta color-primary erbaum-bold space-1">READ MORE</a>
-									</p>
-								</Col>
-							</div>
-						</div>
-						<div className="blogs-section mb-5">
-							<div className="featured-image position-relative overflow-hidden">
-								<a href="#">
-									<img src={blogs1} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out'}}/>
-									<div className="card-layer">
-										<div className="img-icon position-absolute" style={{top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>
-											<i class="fa fa-image" style={{fontSize:'50px', color:'#fff'}}></i>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div className="blogs-content d-flex mt-4 pb-4">
-								<Col sm="1" className="blog-icon text-center border-right pr-0 d-table">
-									<p style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-share-alt"></i><span className="d-block">2</span></p>
-									<p className="border-top border-bottom py-2" style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-envelope"></i><span className="d-block">2</span></p>
-									<p className="mb-0" style={{color:'rgba(0,0,0,0.4)'}}><i class="fa fa-heart"></i><span className="d-block">2</span></p>
-								</Col>
-								<Col sm="11" className="pl-4">
-									<p style={{fontSize:'12px'}}>By <span>Chirofoam</span> In <span>Sleep</span> Posted <span>June 15, 2019</span></p>
-									<h3 className="mb-3"><a href="" className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>4 Benefits of Juicing and How it Helps Improve Sleep</a></h3>
-									<p className="filson-pro-reg" style={{fontSize:'14px'}}>Sleep; the blissful seven to eight hours we spend every night resting cut off from the world sets the tone for the next day. A good rest is a must for having a productive day as sleep helps you [...]</p>
-									<p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
-										<a href="/single-blog/" className="btn-cta color-primary erbaum-bold space-1">READ MORE</a>
-									</p>
-								</Col>
-							</div>
-						</div>
-					</Col>
+					<Blogs />
 					<Col sm="4">
 						<div className="recent-posts">
 							<h5 className="filson-pro-reg">Recent Posts</h5>
