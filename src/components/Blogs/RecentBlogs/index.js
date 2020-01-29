54560eb4462f8272d318442df18e98f173270592
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-01-29 20:20:09 +0530

    Add files via upload

---
+++ src/components/Blogs/RecentBlogs/index.js
@@ -0,0 +1,66 @@
+import React, { useContext } from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import StoreContext from '~/context/StoreContext'
+import blogs1 from "~/assets/img/blogs5.jpg"
+
+const RecentBlogs = () => {
+  const { allShopifyArticle } = useStaticQuery(
+    graphql`
+      query {
+         allShopifyArticle {
+          edges {
+            node {
+              id
+              title
+              excerpt
+              url
+              publishedAt(formatString: "MMM DD, YYYY")
+              image {
+                  src
+                }
+              author {
+                name
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+   
+    
+
+          <Col sm="4">
+            <div className="recent-posts">
+              <h5 className="filson-pro-reg">Recent Posts</h5>
+              <ul className="list-unstyled">
+            {allShopifyArticle.edges
+            ? allShopifyArticle.edges.map(({ node: { id, url, title, content, excerpt, publishedAt, image } }) => (
+            <li className="d-flex mb-3">
+                  <div className="recent-post-image col-4">
+                      <a href=""><img src={image.src} className="img-fluid" alt="Blog w-100" style={{transition:'all 0.15s ease-in-out'}}/></a>
+                  </div>
+                  <div className="recente-posts-data col-8 px-0">
+                    <p className="mb-0"><a href="" style={{fontSize:'12px', color:'#000'}} className="filson-pro-reg d-block mb-0">{title}</a></p>
+                    <span style={{fontSize:'12px'}}>{publishedAt}</span>
+                  </div>
+                </li>
+             ))
+        : <p>No Products found!</p>}
+
+            
+              </ul>
+            </div>
+          </Col>
+          
+       
+  )
+}
+
+export default RecentBlogs
+
+
+
---
+++ src/components/Blogs/RecentPostsFooter/index.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import StoreContext from '~/context/StoreContext'
+
+const RecentPosts = ({ location, RecentPostsId }) => {
+  const { allShopifyArticle } = useStaticQuery(
+    graphql`
+      query {
+         allShopifyArticle {
+          edges {
+            node {
+              id
+              title
+              excerpt
+              url
+              publishedAt(formatString: "MMM DD, YYYY")
+              image {
+                  src
+                }
+              author {
+                name
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+   
+          <Row>
+          {allShopifyArticle.edges
+            ? allShopifyArticle.edges.map(({ node: {id, url, title, content, excerpt, publishedAt, image } }) => (
+          <Col sm="3" className="">
+            <div className="recenet-post-inner border">
+              <a href="#" className="recent-post-image position-relative d-block" style={{height:'200px'}}>
+                <img src={image.src} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out', width:'100%'}}/>
+                <div className="card-layer">
+                </div>
+              </a>
+              <a href="#" className="recent-post-title">
+                <h4 className="text-uppercase erbaum p-2 m-0 border-top" style={{fontSize:'12px', color:'#000'}}>{title}</h4>
+              </a>
+            </div>
+            
+          </Col>
+           ))
+        : <p>No Products found!</p>}
+          </Row>
+          
+       
+  )
+}
+
+export default RecentPosts
