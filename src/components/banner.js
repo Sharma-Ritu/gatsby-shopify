92d4c52a746c25a08dd11bc0025f3394c8183b29
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-02-01 19:51:29 +0530

    Update banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,7 +6,7 @@ import "../assets/css/bootstrap.min.css"
 const Banner = () => (
 	<header className="header-outer home-banner bg-image position-relative">
 		<Header/>
-		<div className="container position-relative">
+		<div className="container-large position-relative">
 		<div className="" style={{color: '#fff',right:'0', left:'0'}}>
 			<div className="">
 				<Row>
@@ -14,7 +14,7 @@ const Banner = () => (
 					
 					</Col>
 					<Col md="6" className="text-center banner-home" style={{right:'0'}}>			
-						<p className="leads filson-pro-reg space-4" style={{fontSize:'20px',paddingLeft:'68px'}}>CHIROFOAM MEMORY FOAM MATTRESS</p>
+						<p className="leads filson-pro-reg space-4 pt-2">CHIROFOAM MEMORY FOAM MATTRESS</p>
 						<h2 className="erbaum-bold space-4 pl-sm-0 pl-xl-4 pl-lg-4" style={{fontStyle:'italic'}}>UPGRADE</h2>
 						<h3 className="erbaum">YOUR SLEEP</h3>
 						<p className="cta mt-0 pt-sm-1 pt-lg-1 pt-xl-1 mb-sm-2 pl-0">
