c32902cf2950daa02982d49c629e52c2567891da
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-02-03 18:25:43 +0530

    Update locations.js

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
@@ -20,7 +21,7 @@ export default (props) => {
 			</Jumbotron>
 		</section>
 		<section className="mb-0 pt-5 pb-3 position-relative">
-			<div className="container-large our-focus pt-5">
+			<Container className="our-focus pt-5">
 				<Row className="mb-2 mb-sm-5 pb-0 pb-sm-5">
 					<Col sm="4" className="text-center px-0 px-sm-4">
 						<h4 className="text-uppercase color-primary erbaum-bold pb-4">Markham</h4>
@@ -83,10 +84,10 @@ export default (props) => {
 						</div>
 					</Col>
 				</Row>
-			</div>
+			</Container>
 		</section>
 		<section>
-			<div className="container-large">
+			<Container>
 				<p className="text-center w-100 filson-pro-reg color-secondary" style={{fontSize:'16px'}}>Also available at:</p>
 				<Row>
 					<Col sm="4" className="col-4 text-center">
@@ -96,33 +97,33 @@ export default (props) => {
 						<a href="https://www.bestbuy.ca/en-ca/category/mattresses-mattress-toppers/mattresses?path=category%253aHome%2BLiving%253bcategory%253aFurniture%253bcategory%253aBedroom%2BFurniture%253bcategory%253aMattresses%2B%2526%2BMattress%2BToppers%253bbrandName%253aCHIROFOAM"><img src={best} alt="Best Buy" width="90%" /></a>
 					</Col>
 					<Col sm="4" className="col-4 text-center">
-						<a href="shop-chirofoam/"><img src={nm} alt="National Mattress" width="90%" /></a>
+						<Link to="/shop-chirofoam/"><img src={nm} alt="National Mattress" width="90%" /></Link>
 					</Col>
 				</Row>
-			</div>
+			</Container>
 		</section>
 		<section>
-			<div className="container-fluid">
+			<Container>
 				<Row className="py-3 py-sm-5 mb-0 mb-sm-5">
 					<div className="w-50 m-auto text-center d-sm-flex">
 						<Col sm="6" className="text-center filson-pro-reg color-secondary mb-5 mb-sm-0">
 							<p className="cta mt-0 pt-sm-0 pt-lg-0 pt-xl-0 mb-sm-2">
-								<a href="/take-test/" className="btn-cta color-primary erbaum-bold space-1">TAKE THE TEST</a>
+								<Link to="/take-test/" className="btn-cta color-primary erbaum-bold space-1">TAKE THE TEST</Link>
 							</p>
 							See if Chirofoam™ is right for you.
 						</Col>
 						<Col sm="6" className="text-center filson-pro-reg color-secondary mb-5 mb-sm-0">
 							<p className="cta mt-0 pt-sm-0 pt-lg-0 pt-xl-0 mb-sm-2">
-								<a href="/shop-chirofoam/" className="btn-cta color-primary erbaum-bold space-1 mr-3">BUY NOW</a>
+								<Link to="/shop-chirofoam/" className="btn-cta color-primary erbaum-bold space-1 mr-3">BUY NOW</Link>
 							</p>
 							Start your journey to a better sleep!
 						</Col>
 					</div>
 				</Row>
-			</div>
+			</Container>
 		</section>
 		<section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
-			<div className="container-fluid pb-0 pb-sm-5">
+			<Container className="pb-0 pb-sm-5">
 				<Row>
 					<p className="text-center w-100 star">
 						<i className="fa fa-star star-small"></i>
@@ -133,11 +134,11 @@ export default (props) => {
 					</p>
 					<p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>“A great quality mattress I enjoy waking up on every day…” <br/>  -Mark F. from Toronto, Ontario</p>
 					<p className="cta mt-0 pt-sm-4 pt-lg-4 pt-xl-4 w-100 text-center mt-4 mt-sm-0">
-						<a href="/reviews/" className="btn-cta color-primary erbaum-bold space-1">SEE REVIEWS</a>
+						<Link to="/reviews/" className="btn-cta color-primary erbaum-bold space-1">SEE REVIEWS</Link>
 					</p>
 					<p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>Chirofoam™ Memory Foam Mattresses are proudly developed and manufactured in Toronto, ON, Canada.</p> 
 				</Row>
-			</div>
+			</Container>
 		</section>
 
 		<Footer />
