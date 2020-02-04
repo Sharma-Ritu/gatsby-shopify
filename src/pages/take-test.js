be016bba79f0b1da1db30a71b84e185b122ee743
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-02-04 20:34:43 +0530

    Update take-test.js

--- src/pages/take-test.js
+++ src/pages/take-test.js
@@ -1,74 +1,320 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import {Container,Jumbotron, Row, Col, Button, Media, Nav, NavItem, NavLink, TabContent, TabPane, Form} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
-import run from "../assets/img/run.png"
 
+const Example = (props) => {
+  const [activeTab, setActiveTab] = useState('1');
 
-export default (props) => {
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+//export default (props) => {
   return (
 	<>
 		<Header />
-		<section>
-			<Jumbotron className="mb-0 text-center color-primary">
-				<h1 className="font-weight-bold display-4 erbaum-bold">Coming Soon...</h1>
-			</Jumbotron>
-		</section>
-		<section className="mb-0 py-5 approved bg-image">
-			<Container>
+		<section className="mb-0 py-3 pb-lg-5 pb-xl-5 position-relative">
+			<Container className="pb-lg-5 pb-xl-5">
 				<Row>
-					<Col sm="4" className="align-middle">
-						<img src={run} alt="run" width="70%"/>
-					</Col>
-					<Col sm="8">
-						<div className="text-white mb-4 mt-4 pt-3 pl-3">
-							<h4 className="lead-text-font text-white erbaum-bold display-5">CUSTOMER APPROVED</h4>
-							<p className="proxima-b space-1col-12 col-sm-7 p-0" style={{fontSize:'14px'}}>Our customers have reported the following benefits after sleeping on there chirofoam mattress</p>
-							<div className="pt-3">
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									97%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improvement in sleep comfort and quality
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									94%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Increased energy in the morning and throughout the day
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									86%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improved muscle recovery and/or relief in daliy experienced back pain
-								  </Media>
-								</Media>
-								<Media className="mt-3">
-								  <Media left middle className="mr-4 h4 erbaum-bold" style={{fontSize:'32px'}}>
-									82%
-								  </Media>
-								  <Media body middle className="proxima-r" style={{fontSize:'1.1rem'}}>
-									  Improvement in sleep comfort and quality
-								  </Media>
-								</Media>
-								<p className="cta erbaum mt-3 mt-sm-0">
-									<a href="#" className="text-white">READ CUSTOMER REVIEW <i className="fa fa-caret-right ic1"></i><i className="fa fa-caret-right ic2"></i><i className="fa fa-caret-right ic3"></i></a>
-								</p>
-							</div>
-						</div>
-					</Col>
+					<h4 className="text-center color-primary erbaum-bold text-uppercase w-100 pt-2 pt-sm-5 mt-0 mt-sm-5">See if the Chirofoam mattress is right for you. Take the test!</h4>
+					<p className="pt-0 text-center w-100 pt-2 pt-lg-4 pt-xl-4 color-secondary" style={{fontSize:'15px'}}>We understand that everyone has different needs and there is no such thing as the perfect mattress that works for everyone. This is why we have created the Chirofoam test to see if the Chirofoam mattress is the right fit for you.</p>
+					<p className="pt-0 text-center w-100 pt-2 color-secondary" style={{fontSize:'15px'}}>Our objective is to provide our customers with a better sleep and a more comfortable mattress than what they currently sleep on. Although the Chirofoam mattress works great for the majority of people, by taking this test you will be able to find out if the Chirofoam mattress is a good fit for you!</p>
+					<p className="cta mt-0 pt-sm-4 pt-lg-5 pt-xl-5 w-100 text-center mt-4 mt-sm-0">
+						<Link to="/reviews/" className="btn-cta color-primary erbaum-bold space-1">START NOW</Link>
+					</p>
 				</Row>
 			</Container>
 		</section>
-
+		<section className="mb-0 py-5 position-relative" id="take-test">
+			<div className="container-large">
+				<div className="col-md-12">
+					<Nav tabs id="tabs" className="d-flex">
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '1' ? 'active' : ''}  onClick={() => setActiveTab('1')}>
+								<span>Step 1</span>
+								Sleep Position
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '2' ? 'active' : ''} onClick={() => setActiveTab('2')}>
+								<span>Step 2</span>
+								Surface
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '3' ? 'active' : ''} onClick={() => setActiveTab('3')}>
+								<span>Step 3</span>
+								Body Type
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '4' ? 'active' : ''} onClick={() => setActiveTab('4')}>
+								<span>Step 4</span>
+								Temperature
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '5' ? 'active' : ''} onClick={() => setActiveTab('5')}>
+								<span>Step 5</span>
+								Exercise
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink nav-link small color-secondary filson-pro-reg className={activeTab == '6' ? 'active' : ''} onClick={() => setActiveTab('6')}>
+								<span>Step 6</span>
+								Back Pain
+							</NavLink>
+						</NavItem>
+					</Nav>
+					<TabContent className="take-test-tab" activeTab={activeTab}>
+						<TabPane tabId="1">
+							<div id="tabsContent" className="tab-content">
+                    			<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+												<div className="left-content">
+													<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">1. In what position do you most frequently sleep at night?</h3>
+													<div>
+														<input type="radio" value="1" name="first_stepopt" id="radioa1"/>
+														<label className="opt_heading" for="radioa1">a. I sleep on my side.</label>
+													</div>
+													<div>
+														<input type="radio" value="2" name="first_stepopt" id="radioa2"/>
+														<label className="opt_heading" for="radioa2">b. I sleep on my back and/or stomach.</label>
+													</div>
+													<div>
+														<input type="radio" value="3" name="first_stepopt" id="radioa3"/>
+														<label className="opt_heading" for="radioa3">c. I toss and turn all the time.</label>
+													</div>
+												</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+									</Row>
+								</div>
+							</div>
+						</TabPane>
+						<TabPane tabId="2">
+							<div id="tabsContent" className="tab-content border border-top-0">
+								<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+											<div className="left-content">
+												<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">2. On which surface do you think you would have the most comfortable sleep?</h3>
+												<div>
+													<input type="radio" value="1" name="second_stepopt" id="radiob1" />
+													<label className="opt_heading" for="radiob1">a. Soft – Like sleeping in a big pile of pillows that you sink into.</label>
+												</div>
+												<div>
+													<input type="radio" value="2" name="second_stepopt" id="radiob2" />
+													<label className="opt_heading" for="radiob2">b. Medium – Like sleeping on the couch with regular cushions.</label>
+												</div>
+												<div>
+													<input type="radio" value="3" name="second_stepopt" id="radiob3" />
+													<label className="opt_heading" for="radiob3">c. Firm – Like sleeping on the floor with a thick and fluffy comforter underneath.</label>
+												</div>
+												<div>
+													<input type="radio" value="4" name="second_stepopt" id="radiob4" />
+													<label className="opt_heading" for="radiob4">d. Extra Firm – Like sleeping on the bare floor with just a carpet underneath.</label>
+												</div>
+											</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+                    				</Row>
+                				</div>
+            				</div>
+						</TabPane>
+						<TabPane tabId="3">
+							<div id="tabsContent" className="tab-content border border-top-0">
+								<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+											<div className="left-content">
+												<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">3. What would you consider your body type?</h3>
+												<div>
+													<input type="radio" value="1" name="third_stepopt" id="radioc1"/>
+													<label className="opt_heading" for="radioc1">a. Small – I am very small and/or very skinny. (Typically under 130lbs.)</label>
+												</div>
+												<div>
+													<input type="radio" value="2" name="third_stepopt" id="radioc2"/>
+													<label className="opt_heading" for="radioc2">b. Average – I am average height and average build. (Between 130lbs and 210lbs.)</label>
+												</div>
+												<div>
+													<input type="radio" value="3" name="third_stepopt" id="radioc3"/>
+													<label className="opt_heading" for="radioc3">c. Large – I am taller than average and/or a little heavier than average. (Between 210lbs and 280lbs.)</label>
+												</div>
+												<div>
+													<input type="radio" value="4" name="third_stepopt" id="radioc4"/>
+													<label className="opt_heading" for="radioc4">d. Extra Large – I have a very large frame and on the heavy side. (Over 280lbs.)</label>
+												</div>
+											</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+                    				</Row>
+                				</div>
+            				</div>
+						</TabPane>
+						<TabPane tabId="4">
+							<div id="tabsContent" className="tab-content border border-top-0">
+								<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+											<div className="left-content">
+												<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">4. Do you get hot and sweaty or very cold when you sleep at night?</h3>
+												<div>
+													<input type="radio" value="1" name="forth_stepopt" id="radiod1" />
+													<label className="opt_heading" for="radiod1">a. I sleep very cool and always need to wrap myself up to keep warm.</label>
+												</div>
+												<div>
+													<input type="radio" value="2" name="forth_stepopt" id="radiod2" />
+													<label className="opt_heading" for="radiod2">b. My sleep temperature is not a problem, I’m never too hot or too cold.</label>
+												</div>
+												<div>
+													<input type="radio" value="3" name="forth_stepopt" id="radiod3" />
+													<label className="opt_heading" for="radiod3">c. I get hot once in a while but nothing drastic.</label>
+												</div>
+												<div>
+													<input type="radio" value="4" name="forth_stepopt" id="radiod4" />
+													<label className="opt_heading" for="radiod4">d. I get hot and sweaty when I sleep and wake up in the middle of the night constantly.</label>
+												</div>
+											</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+                    				</Row>
+                				</div>
+            				</div>
+						</TabPane>
+						<TabPane tabId="5">
+							<div id="tabsContent" className="tab-content border border-top-0">
+								<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+											<div className="left-content">
+												<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">5. How often to you play sports, work out, exercise, and/or go for a run?</h3>
+												<div>
+													<input type="radio" value="1" name="fifth_stepopt" id="radioe1" />
+													<label className="opt_heading" for="radioe1">a. 3-7 times a week.</label>
+												</div>
+												<div>
+													<input type="radio" value="2" name="fifth_stepopt" id="radioe2" />
+													<label className="opt_heading" for="radioe2">b. 1-2 times a week.</label>
+												</div>
+												<div>
+													<input type="radio" value="3" name="fifth_stepopt" id="radioe3" />
+													<label className="opt_heading" for="radioe3">c. A few times a month.</label>
+												</div>
+												<div>
+													<input type="radio" value="4" name="fifth_stepopt" id="radioe4" />
+													<label className="opt_heading" for="radioe4">d. Never.</label>
+												</div>
+											</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+										
+                    				</Row>
+                				</div>
+            				</div>
+						</TabPane>
+						<TabPane tabId="6">
+							<div id="tabsContent" className="tab-content border border-top-0">
+								<div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+									<Row className="m-0">
+										<Col className="col-12 col-lg-7 col-xl-7">
+											<div className="left-content">
+												<h3 className="head_point filson-pro-reg pb-2 pb-lg-4 pb-xl-4">6. Do you suffer from back pain?</h3>
+												<div>
+													<input type="radio" value="1" name="sixth_stepopt" id="radiof1" />
+													<label className="opt_heading" for="radiof1">a. I suffer from severe back pain constantly.</label>
+												</div>
+												<div>
+													<input type="radio" value="2" name="sixth_stepopt" id="radiof2" />
+													<label className="opt_heading" for="radiof2">b. I get occasional back pain that is severe.</label>
+												</div>
+												<div>
+													<input type="radio" value="3" name="sixth_stepopt" id="radiof3" />
+													<label className="opt_heading" for="radiof3">c. I get occasional back pain that is mild.</label>
+												</div>
+												<div>
+													<input type="radio" value="4" name="sixth_stepopt" id="radiof4" />
+													<label className="opt_heading" for="radiof4">d. I do not suffer from back pain.</label>
+												</div>
+											</div>
+										</Col>
+										<Col className="col-12 col-lg-5 col-xl-5">
+											<div>
+												
+											</div>
+										</Col>
+                    				</Row>
+                				</div>
+            				</div>
+						</TabPane>
+					</TabContent>
+				</div>
+			</div>
+		</section>
+		<section>
+			<Container>
+				<Row className="py-3 py-sm-5 mb-0 mb-sm-5">
+					<div className="w-50 m-auto text-center d-sm-flex">
+						<Col sm="6" className="text-center filson-pro-reg color-secondary mb-5 mb-sm-0">
+							<p className="cta mt-0 pt-sm-0 pt-lg-0 pt-xl-0 mb-sm-2">
+								<Link to="/take-test/" className="btn-cta color-primary erbaum-bold space-1">TAKE THE TEST</Link>
+							</p>
+							See if Chirofoam™ is right for you.
+						</Col>
+						<Col sm="6" className="text-center filson-pro-reg color-secondary mb-5 mb-sm-0">
+							<p className="cta mt-0 pt-sm-0 pt-lg-0 pt-xl-0 mb-sm-2">
+								<Link to="/shop-chirofoam/" className="btn-cta color-primary erbaum-bold space-1 mr-3">BUY NOW</Link>
+							</p>
+							Start your journey to a better sleep!
+						</Col>
+					</div>
+				</Row>
+			</Container>
+		</section>
+		<section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
+			<Container className="pb-0 pb-sm-5">
+				<Row>
+					<p className="text-center w-100 star">
+						<i className="fa fa-star star-small"></i>
+						<i className="fa fa-star star-medium ml-2"></i>
+						<i className="fa fa-star star-large mx-2"></i>
+						<i className="fa fa-star star-medium mr-2"></i>
+						<i className="fa fa-star star-small"></i>
+					</p>
+					<p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>“A great quality mattress I enjoy waking up on every day…” <br/>  -Mark F. from Toronto, Ontario</p>
+					<p className="cta mt-0 pt-sm-4 pt-lg-4 pt-xl-4 w-100 text-center mt-4 mt-sm-0">
+						<Link to="/reviews/" className="btn-cta color-primary erbaum-bold space-1">SEE REVIEWS</Link>
+					</p>
+					<p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>Chirofoam™ Memory Foam Mattresses are proudly developed and manufactured in Toronto, ON, Canada.</p> 
+				</Row>
+			</Container>
+		</section>
+    
 		<Footer />
 	</>
   );
 };
+export default Example;
