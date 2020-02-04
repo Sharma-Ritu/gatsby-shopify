e538079239c3ba45c71636d2cbcb61f555b57a33
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-04 15:16:16 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/pages/single-blog.js
+++ src/pages/single-blog.js
@@ -6,6 +6,7 @@ import "../assets/css/bootstrap.min.css"
 import blogs1 from "../assets/img/blogs5.jpg"
 import girl from "../assets/img/girl.jpg"
 import RecentPosts from "../components/Blogs/RecentPostsFooter"
+import SingleArticle from "../components/Blogs/SingleArticle"
 /*import SingleBlogs from "../components/Blogs/SingleBlogs"*/
 
 export default ({data}) => {
