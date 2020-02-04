c54aa34716f2ea26920d40e9b41c38e87ef12338
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-04 23:04:55 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Blogs/RecentPostsFooter/index.js
+++ src/components/Blogs/RecentPostsFooter/index.js
@@ -35,12 +35,12 @@ const RecentPosts = ({ location, RecentPostsId }) => {
             ? allShopifyArticle.edges.map(({ node: {id, url, title, content, excerpt, publishedAt, image } }) => (
           <Col sm="3" className="" key={id}>
             <div className="recenet-post-inner border">
-              <Link to="/article/${id}/" className="recent-post-image position-relative d-block" style={{height:'200px'}}>
+              <Link to={`/article/${id}/`} className="recent-post-image position-relative d-block" style={{height:'200px'}}>
                 <img src={image.src} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out', width:'100%'}}/>
                 <div className="card-layer">
                 </div>
               </Link>
-              <Link to="/article/${id}/" className="recent-post-title">
+              <Link to={`/article/${id}/`} className="recent-post-title">
                 <h4 className="text-uppercase erbaum p-2 m-0 border-top" style={{fontSize:'12px', color:'#000'}}>{title}</h4>
               </Link>
             </div>
