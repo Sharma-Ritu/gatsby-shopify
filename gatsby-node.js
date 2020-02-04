4337687db00e8cf70fa6b4a5218be68df9d980d7
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-04 17:10:02 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,7 +21,6 @@ exports.createPages = ({ graphql, actions }) => {
       }
     }
   `).then(result => {
-    console.log(result)
     result.data.allShopifyProduct.edges.forEach(({ node }) => {
       createPage({
         path: `/product/${node.handle}/`,
@@ -35,35 +34,6 @@ exports.createPages = ({ graphql, actions }) => {
     })
     result.data.allShopifyArticle.edges.forEach(({ node }) => {
       createPage({
-        path: `/article/${node.id}/`,
-        component: path.resolve(`./src/templates/ArticlePage/index.js`),
-        context: {
-          // Data passed to context is available
-          // in article queries as GraphQL variables.
-          id: node.id,
-        },
-      })
-    })
-  })
-}
-
-exports.createArticles = ({ graphql, actions }) => {
-  const { createArticle } = actions
-  return graphql(`
-    {
-      allShopifyArticle {
-        edges {
-          node {
-            id
-            url
-          }
-        }
-      }
-    }
-  `).then(result => {
-    console.log(result)
-    result.data.allShopifyArticle.edges.forEach(({ node }) => {
-      createArticle({
         path: `/article/${node.id}/`,
         component: path.resolve(`./src/templates/ArticlePage/index.js`),
         context: {
