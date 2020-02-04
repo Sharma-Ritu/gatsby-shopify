ce172d11ee569ea5cd95c90eb5e4068da2153b8d
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-02-04 20:47:33 +0530

    Update index.js

--- src/components/Blogs/RecentPostsFooter/index.js
+++ src/components/Blogs/RecentPostsFooter/index.js
@@ -7,7 +7,7 @@ const RecentPosts = ({ location, RecentPostsId }) => {
   const { allShopifyArticle } = useStaticQuery(
     graphql`
       query {
-         allShopifyArticle (sort: {order: DESC, fields: publishedAt}, limit: 4){
+         allShopifyArticle (sort: {order: DESC, fields: publishedAt}, limit: 3){
           edges {
             node {
               id
