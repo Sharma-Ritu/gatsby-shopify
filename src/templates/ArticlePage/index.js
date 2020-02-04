53797bec7a80c9169f9a201f8fbaf888b0abd0b4
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-04 17:14:15 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/ArticlePage/index.js
+++ src/templates/ArticlePage/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from "../components/header"
-import Footer from "../components/footer"
+import Header from "../../components/header"
+import Footer from "../../components/footer"
 import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
-import "../assets/css/bootstrap.min.css"
-import blogs1 from "../assets/img/blogs5.jpg"
-import girl from "../assets/img/girl.jpg"
-import RecentPosts from "../components/Blogs/RecentPostsFooter"
+import "../../assets/css/bootstrap.min.css"
+import blogs1 from "../../assets/img/blogs5.jpg"
+import girl from "../../assets/img/girl.jpg"
+import RecentPosts from "../../components/Blogs/RecentPostsFooter"
 
 const ArticlePage = ({ data }) => {
   const article = data.shopifyArticle
