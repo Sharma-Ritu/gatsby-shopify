05e915bda0c14ff6c02805ac1a71ebe77621f551
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-04 17:55:33 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Blogs/index.js
+++ src/components/Blogs/index.js
@@ -38,7 +38,7 @@ const Blogs = ({ id }) => {
             ? allShopifyArticle.edges.map(({ node: { id, url, title, content, excerpt, publishedAt, image } }) => (
             <div className="blogs-section mb-5">
               <div className="featured-image position-relative overflow-hidden">
-                <Link to={`/single-blog/?id=${id}`} state={{ fromFeed: true }}>
+                <Link to={`/article/${id}/`} state={{ fromFeed: true }}>
                   <img src={image.src} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out'}}/>
                   <div className="card-layer">
                     <div className="img-icon position-absolute" style={{top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>
@@ -55,10 +55,10 @@ const Blogs = ({ id }) => {
                 </Col>
                 <Col sm="11" className="pl-4">
                   <p style={{fontSize:'12px'}}>By <span>Chirofoam</span> In <span>Sleep</span> Posted <span> {publishedAt}</span></p>
-                  <h3 className="mb-3"><Link to={`/single-blog/?id=${id}`} state={{ fromFeed: true }} className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>{title}</Link></h3>
+                  <h3 className="mb-3"><Link to={`/article/${id}/`} state={{ fromFeed: true }} className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>{title}</Link></h3>
                   <p className="filson-pro-reg" style={{fontSize:'14px'}}>{excerpt}</p>
                   <p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
-                    <Link to={`/single-blog/?id=${id}`} state={{ fromFeed: true }} className="btn-cta color-primary erbaum-bold space-1">READ MORE</Link>
+                    <Link to={`/article/${id}/`} state={{ fromFeed: true }} className="btn-cta color-primary erbaum-bold space-1">READ MORE</Link>
                   </p>
                 </Col>
               </div>
