e2c2b81f862f43f129769cf58d48ee3b24f77c26
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-05 12:50:52 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "../components/header"
 import Footer from "../components/footer"
 import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import SEO from '~/components/seo'
 import "../assets/css/bootstrap.min.css"
 import blogs1 from "../assets/img/blogs5.jpg"
 import Blogs from "../components/Blogs"
@@ -11,6 +12,7 @@ import RecentBlogs from "../components/Blogs/RecentBlogs"
 export default (props) => {
   return (
 	<>
+		<SEO title="Chirofoam™ Memory Foam Mattresses" description="Sleep Education and Chirofoam™ News" />
 		<Header />
 		<section className="blue-bg charity">
 			<Jumbotron className="mb-0 text-center text-white bg-transparent space-1 w-100 m-auto">
