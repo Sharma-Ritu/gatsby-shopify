d0de5c0118d40e73e245cfbeeaa325656ae3111d
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-05 13:05:40 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Blogs/BlogPagination/index.js
+++ src/components/Blogs/BlogPagination/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 const BlogPagination = (props) => {
-  console.log(props);
+  console.log(props.pageInfo);
   return (
-    <Pagination aria-label="Page navigation example">
-      <PaginationItem disabled>
+    <Pagination aria-label="Page navigation">
+      <PaginationItem {props.pageInfo.hasPreviousPage && disabled}>
         <PaginationLink first href="#" />
       </PaginationItem>
-      <PaginationItem disabled>
+      <PaginationItem {props.pageInfo.hasPreviousPage && disabled}>
         <PaginationLink previous href="#" />
       </PaginationItem>
       <PaginationItem active>
@@ -19,21 +19,6 @@ const BlogPagination = (props) => {
       <PaginationItem>
         <PaginationLink href="#">
           2
-        </PaginationLink>
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#">
-          3
-        </PaginationLink>
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#">
-          4
-        </PaginationLink>
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#">
-          5
         </PaginationLink>
       </PaginationItem>
       <PaginationItem>
