a07f77f8209bb778fa4d95e967a9df2cc5171248
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-06 09:11:44 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/BlogPage/index.js
+++ src/templates/BlogPage/index.js
@@ -20,6 +20,31 @@ const BlogPage = ({ data }) => {
         <p className="space-4 proxima-r text-white m-auto text-center px-0 px-sm-5 px-lg-5 px-xl-5 pt-2 pt-sm-4" style={{width:'90%',fontSize:'26px'}}>Sleep Education and Chirofoam™ News</p>
       </Jumbotron>
     </section>
+    <section className="mb-0 py-5 position-relative">
+      <div className="container-large">
+        <Row>
+          <RecentBlogs />
+        </Row>
+      </div>
+    </section>
+    <section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
+      <div className="container-fluid pb-0 pb-sm-5">
+        <Row>
+          <p className="text-center w-100 star">
+            <i className="fa fa-star star-small"></i>
+            <i className="fa fa-star star-medium ml-2"></i>
+            <i className="fa fa-star star-large mx-2"></i>
+            <i className="fa fa-star star-medium mr-2"></i>
+            <i className="fa fa-star star-small"></i>
+          </p>
+          <p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>“A great quality mattress I enjoy waking up on every day…” <br/>  -Mark F. from Toronto, Ontario</p>
+          <p className="cta mt-0 pt-sm-4 pt-lg-4 pt-xl-4 w-100 text-center mt-4 mt-sm-0">
+            <a href="/reviews/" className="btn-cta color-primary erbaum-bold space-1">SEE REVIEWS</a>
+          </p>
+          <p className="filson-pro-reg color-secondary pt-3 w-75 m-auto text-center space-1" style={{fontSize:'20px'}}>Chirofoam™ Memory Foam Mattresses are proudly developed and manufactured in Toronto, ON, Canada.</p> 
+        </Row>
+      </div>
+    </section>
     <Footer />
     </>
   )
