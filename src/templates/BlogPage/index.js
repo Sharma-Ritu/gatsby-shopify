931ed685f71f0721f4229d9c52e8337922df7b17
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-06 09:23:31 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/BlogPage/index.js
+++ src/templates/BlogPage/index.js
@@ -9,7 +9,9 @@ import Blogs from "~/components/Blogs"
 import RecentBlogs from "~/components/Blogs/RecentBlogs"
 
 const BlogPage = ({ data }) => {
-  console.log(data.allShopifyArticle);
+  const allShopifyArticle = data.allShopifyArticle;
+  const pageInfo = allShopifyArticle.pageInfo;
+  console.log(allShopifyArticle, pageInfo);
   return (
     <>
     <SEO title="Chirofoam™ Memory Foam Mattresses" description="Sleep Education and Chirofoam™ News" />
@@ -23,6 +25,67 @@ const BlogPage = ({ data }) => {
     <section className="mb-0 py-5 position-relative">
       <div className="container-large">
         <Row>
+          <Col sm="8" className="align-middle">
+            {allShopifyArticle.edges
+            ? allShopifyArticle.edges.map(({node: { id, url, title, content, excerpt, publishedAt, image, author, blog}}, index) => (
+            <div className="blogs-section mb-4" key={id}>
+              <div className="featured-image position-relative overflow-hidden">
+                <Link to={`/article/${id}/`} state={{ fromFeed: true }}>
+                  <img src={image.src} className="img-fluid" alt="Blog" style={{transition:'all 0.15s ease-in-out'}}/>
+                  <div className="card-layer">
+                    <div className="img-icon position-absolute" style={{top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>
+                      <i className="fa fa-image" style={{fontSize:'50px', color:'#fff'}}></i>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <div className="blogs-content d-flex mt-4">
+                <Col sm="1" className="blog-icon text-center border-right pr-0 d-table">
+                  <p style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-share-alt"></i><span className="d-block">2</span></p>
+                  <p className="border-top border-bottom py-2" style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-envelope"></i><span className="d-block">2</span></p>
+                  <p className="mb-0" style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-heart"></i><span className="d-block">2</span></p>
+                </Col>
+                <Col sm="11" className="pl-4">
+                  <p style={{fontSize:'12px'}}>By <span>{author.name}</span> In <span>{blog.title}</span> Posted <span> {publishedAt}</span></p>
+                  <h3 className="mb-3"><Link to={`/article/${id}/`} state={{ fromFeed: true }} className="text-uppercase erbaum space-1" style={{color:'#000', fontSize:'24px'}}>{title}</Link></h3>
+                  <p className="filson-pro-reg" style={{fontSize:'14px'}}>{excerpt}</p>
+                  <p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
+                    <Link to={`/article/${id}/`} state={{ fromFeed: true }} className="btn-cta color-primary erbaum-bold space-1">READ MORE</Link>
+                  </p>
+                </Col>
+              </div>
+            </div>
+             ))
+        : <p>No Products found!</p>}
+            {(pageInfo.pageCount > 1) &&
+              <Pagination aria-label="Page navigation">
+                <PaginationItem disabled={!pageInfo.hasPreviousPage}>
+                  <PaginationLink
+                    previous
+                    href={"#" + (currentPage - 1)}
+                    onClick={e => handlePagination(e, previousPage)}
+                  />
+                </PaginationItem>
+                {[...Array(pageInfo.pageCount)].map((page, i) => 
+                  <PaginationItem active={(i+1) === currentPage} key={i}>
+                    <PaginationLink
+                      onClick={e => handlePagination(e, (i + 1))}
+                      href={"#" + (i + 1)}
+                    >
+                      {i + 1}
+                    </PaginationLink>
+                  </PaginationItem>
+                )}
+                <PaginationItem disabled={!pageInfo.hasNextPage}>
+                  <PaginationLink
+                    next
+                    href={"#" + (currentPage + 1)}
+                    onClick={e => handlePagination(e, nextPage)}
+                  />
+                </PaginationItem>
+              </Pagination>
+            }
+          </Col>
           <RecentBlogs />
         </Row>
       </div>
