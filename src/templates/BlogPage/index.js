437cf8c5e26506802d4ec61e428da39dd9ed3bd4
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-06 08:56:52 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/BlogPage/index.js
+++ src/templates/BlogPage/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from "~/components/header"
 import Footer from "~/components/footer"
+import {Container,Jumbotron, Row, Col, Button, Media} from 'reactstrap';
+import SEO from '~/components/seo'
 import "~/assets/css/bootstrap.min.css"
+import blogs1 from "../assets/img/blogs5.jpg"
+import Blogs from "../components/Blogs"
+import RecentBlogs from "../components/Blogs/RecentBlogs"
 
 const BlogPage = ({ data }) => {
-  console.log(data);
+  console.log(data.allShopifyArticle);
   return (
     <>
+    <SEO title="Chirofoam™ Memory Foam Mattresses" description="Sleep Education and Chirofoam™ News" />
     <Header />
+    <section className="blue-bg charity">
+      <Jumbotron className="mb-0 text-center text-white bg-transparent space-1 w-100 m-auto">
+        <h2 className="font-weight-bold erbaum-bold text-uppercase pt-md-5 space-2 pt-sm-1">Chirofoam™ Memory Foam Mattresses</h2>
+        <p className="space-4 proxima-r text-white m-auto text-center px-0 px-sm-5 px-lg-5 px-xl-5 pt-2 pt-sm-4" style={{width:'90%',fontSize:'26px'}}>Sleep Education and Chirofoam™ News</p>
+      </Jumbotron>
+    </section>
     <Footer />
     </>
   )
