fe0606920c6374f1602033421256c0ab5d8c21cd
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-07 15:48:00 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/pages/take-test.js
+++ src/pages/take-test.js
@@ -61,18 +61,13 @@ const TAKETEST = (props) => {
   	}
   }
   const processTest = (event) => {
-  	const Index = selectedChoices.findIndex((step => step.id === 1));
-  	const Step1 = selectedChoices[Index];
-  	Index = selectedChoices.findIndex((step => step.id === 2));
-  	const Step2 = selectedChoices[Index];
-  	Index = selectedChoices.findIndex((step => step.id === 3));
-  	const Step3 = selectedChoices[Index];
-  	Index = selectedChoices.findIndex((step => step.id === 4));
-  	const Step4 = selectedChoices[Index];
-  	Index = selectedChoices.findIndex((step => step.id === 5));
-  	const Step5 = selectedChoices[Index];
-  	Index = selectedChoices.findIndex((step => step.id === 6));
-  	const Step6 = selectedChoices[Index];
+  	const Step1 = selectedChoices[selectedChoices.findIndex((step => step.id === 1))];
+  	const Step2 = selectedChoices[selectedChoices.findIndex((step => step.id === 2)];
+  	const Step3 = selectedChoices[selectedChoices.findIndex((step => step.id === 3)];
+  	const Step4 = selectedChoices[selectedChoices.findIndex((step => step.id === 4)];
+  	const Step5 = selectedChoices[selectedChoices.findIndex((step => step.id === 5)];
+  	const Step6 = selectedChoices[selectedChoices.findIndex((step => step.id === 6)];
+  	console.log(Step1, Step2, Step3, Step4, Step5, Step6);
   	if((Step2.choice === 1) || (Step3.choice === 1)){
   		console.log('sorry')
   	}else if((Step2.choice === 4) || (Step6.choice === 1) || ((Step2.choice === 3)&&(Step3.choice === 4))){
