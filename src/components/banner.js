c59977cbed292bc2622adffe5ef40c052e24394a
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-02-10 19:13:58 +0530

    Update banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -18,7 +18,7 @@ const Banner = () => (
 						<p className="leads filson-pro-reg space-4 pt-2 animated fadeInDown slow">CHIROFOAM MEMORY FOAM MATTRESS</p>
 						<h2 className="erbaum-bold space-4 pl-sm-0 pl-xl-4 pl-lg-4 animated fadeInRight" style={{fontStyle:'italic'}}>UPGRADE</h2>
 						<h3 className="erbaum animated fadeInUp slow">YOUR SLEEP</h3>
-						<p className="cta mt-0 pt-sm-1 pt-lg-1 pt-xl-1 mb-sm-2 pl-0 animated fadeInLeft">
+						<p className="cta mt-0 pt-sm-1 pt-lg-1 pt-xl-1 mb-sm-2 pl-0 animated shake">
 							<a href="/shop-chirofoam/" className="btn-cta text-white erbaum-bold space-1">BUY NOW</a>
 						</p>
 					</Col>
