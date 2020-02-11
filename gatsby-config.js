4e629938354222cbeb0cb2b533ddba78dc7446ef
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-11 17:43:52 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#243b6b`,
+        theme_color: `#243b6b`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
