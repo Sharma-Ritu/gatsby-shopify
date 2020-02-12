fdf0328fe9b9285f4f7a1b09ee9ad8cded0c132f
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-02-12 18:41:22 +0530

    Update banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,30 +3,19 @@ import Header from '../components/header'
 import {Row, Col} from 'reactstrap';
 import "../assets/css/bootstrap.min.css"
 import "../assets/css/animate.css"
+import HomePageSlider from '../components/HomePageSlider'
 
-const Banner = () => (
-	<header className="header-outer home-banner bg-image position-relative">
+const Banner = () => {
+
+	return(
+	<header className="header-outer">
+		
+		<div className="banner-slider position-relative">
+			<HomePageSlider />
+		</div>
 		<Header/>
-		<div className="container-large position-relative">
-		<div className="" style={{color: '#fff',right:'0', left:'0'}}>
-			<div className="">
-				<Row>
-					<Col md="6">
-					
-					</Col>
-					<Col md="6" className="text-center banner-home" style={{right:'0'}}>			
-						<p className="leads filson-pro-reg space-4 pt-2 animated fadeInDown slow">CHIROFOAM MEMORY FOAM MATTRESS</p>
-						<h2 className="erbaum-bold space-4 pl-sm-0 pl-xl-4 pl-lg-4 animated fadeInRight" style={{fontStyle:'italic'}}>UPGRADE</h2>
-						<h3 className="erbaum animated fadeInUp slow">YOUR SLEEP</h3>
-						<p className="cta mt-0 pt-sm-1 pt-lg-1 pt-xl-1 mb-sm-2 pl-0 animated fadeInLeft">
-							<a href="/shop-chirofoam/" className="btn-cta text-white erbaum-bold space-1">BUY NOW</a>
-						</p>
-					</Col>
-				</Row>
-			</div>
-		</div>
-		</div>
 	</header>
-)
+	)
+}
 
 export default Banner;
