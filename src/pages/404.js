ab19256b84f6c6ba9095a8b0779a537ec780ab37
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-02-12 19:44:51 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import {Container,Jumbotron, Row, Col, Card, CardBody, UncontrolledCollapse} from 'reactstrap';
+import "../assets/css/bootstrap.min.css"
 
-import SEO from '~/components/seo'
+const FourZeroFour = (props) => {
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
-
-export default NotFoundPage
+	return (
+	<>
+		<Header />
+		<section className="error-page py-5">
+			<Container>
+					<div className="four-zero-four text-center">
+						<strong className="filson-pro-reg py-3">WHOOPS!</strong>
+						<h1 className="proxima-r pt-3 color-primary">404</h1>
+						<p className="proxima-b pt-3">Sorry! Page not found.</p>
+						<p className="cta mt-0 pt-sm-4 pt-lg-4 pt-xl-4 w-100 text-center mt-4 mt-sm-0">
+							<Link to="" className="btn-cta color-primary erbaum-bold space-1">GO TO HOMEPAGE</Link>
+						</p>
+					</div>
+			</Container>
+		</section>
+		
+		
+		<Footer />
+	</>
+  );
+}
+export default FourZeroFour
