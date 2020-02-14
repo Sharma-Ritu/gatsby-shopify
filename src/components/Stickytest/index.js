3c95d2b419d096dd1b23dd57e3ed186d81f29687
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-14 17:12:56 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Stickytest/index.js
+++ src/components/Stickytest/index.js
@@ -3,15 +3,18 @@ import {Link} from "gatsby"
 import {Row, Col} from 'reactstrap';
 
 const Stickytest = () => {
-	return(){
+	const toggleTakeTest = () => {
+		console.log('here')
+	}
+	return(
 		<div className="sticky-test position-fixed">
 			<div className="test-content-slider-tab">
 				<div className="test-content-slider-title">
-					<button className="fs-2">Take The Chirofoam Test</button>
+					<button className="fs-2" onClick={toggleTakeTest}>Take The Chirofoam Test</button>
 				</div>
 			</div>
 		</div>
-	}
+	)
 }
 
 export default Stickytest;
