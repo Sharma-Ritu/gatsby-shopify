a1e311cc3b776b93d096d05f1f8e51c4970d4bd2
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-14 17:31:44 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Stickytest/index.js
+++ src/components/Stickytest/index.js
@@ -14,7 +14,7 @@ const Stickytest = () => {
 				</div>
 				<div className="test-slider-content hidden">
 					<div className="test-slider-content-inner-wrap mCustomScrollbar _mCS_1 mCS_no_scrollbar">
-						<div id="mCSB_1" className="mCustomScrollBox mCS-dark mCSB_vertical mCSB_inside" tabindex="0">
+						<div id="mCSB_1" className="mCustomScrollBox mCS-dark mCSB_vertical mCSB_inside">
 							<div id="mCSB_1_container" className="mCSB_container mCS_y_hidden mCS_no_scrollbar_y">
 								<h4><span>Take The Chirofoam Test</span></h4>
 								<p><span>See if Chirofoam is right for you.</span></p>
@@ -26,7 +26,7 @@ const Stickytest = () => {
 							</div>
 						</div>
 					</div>
-				<span class="test-close-slider-content">x</span>
+				<span className="test-close-slider-content">x</span>
 				</div>
 			</div>
 		</div>
