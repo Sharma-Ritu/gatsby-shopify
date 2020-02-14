2e75893ededebc499cdcb868dd59eeeb062fcef2
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-14 17:46:33 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Stickytest/index.js
+++ src/components/Stickytest/index.js
@@ -17,18 +17,12 @@ const Stickytest = () => {
 					<button className="fs-2" onClick={openTakeTest}>Take The Chirofoam Test</button>
 				</div>
 				<div className="test-slider-content hidden">
-					<div className="test-slider-content-inner-wrap mCustomScrollbar _mCS_1 mCS_no_scrollbar">
-						<div id="mCSB_1" className="mCustomScrollBox mCS-dark mCSB_vertical mCSB_inside">
-							<div id="mCSB_1_container" className="mCSB_container mCS_y_hidden mCS_no_scrollbar_y">
-								<h4><span>Take The Chirofoam Test</span></h4>
-								<p><span>See if Chirofoam is right for you.</span></p>
-								<p className="start_noww">
-									<span>
-										<Link to="/take-test/">START NOW</Link>
-									</span>
-								</p>
-							</div>
-						</div>
+					<div className="text-center">
+						<h4><span>Take The Chirofoam Test</span></h4>
+						<p><span>See if Chirofoam is right for you.</span></p>
+						<p className="start_noww">
+							<Link className="btn text-white btn-custom-secondary" to="/take-test/">START NOW</Link>
+						</p>
 					</div>
 				<button className="test-close-slider-content" onClick={closeTakeTest}>&times;</button>
 				</div>
