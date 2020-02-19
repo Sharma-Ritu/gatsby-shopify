d29d51d17c50b5439e4e84b0f25c3b000beaf430
rajkumar-inovexia <rajkumar@innovagesoftwares.com>
2020-02-19 19:43:48 +0530

    Signed-off-by: rajkumar-inovexia <rajkumar@innovagesoftwares.com>

--- src/components/FiveKeys/index.js
+++ src/components/FiveKeys/index.js
@@ -47,7 +47,7 @@ const SlickSlider =()=> {
                   <h3 className="pt-0 pt-sm-3 text-right text-white pr-3 erbaum-bold">01</h3>
                   <div className="card-data text-white position-absolute p-3">
                     <h4 className="erbaum-bold pr-0 pr-sm-0 pr-lg-2 pf-xl-5 text-uppercase"  style={{fontSize:'18px'}}>PRESSURE RELIEF FOAM</h4>
-                    <p className="proxima-eb text-1" style={{letterSpacing:'1px'}}>The Chirofoam™ memory foam mattress is designed to relieve pressure which is built up in your body from daily activities. This pressure build up is largely responsible for common back pain experienced by many people, and can be caused by improper sitting posture, heavy lifting, or other strenuous activities.</p>
+                    <p className="proxima-r text-1" style={{letterSpacing:'1px'}}>The Chirofoam™ memory foam mattress is designed to relieve pressure which is built up in your body from daily activities. This pressure build up is largely responsible for common back pain experienced by many people, and can be caused by improper sitting posture, heavy lifting, or other strenuous activities.</p>
                   </div>
                 </div>
               </div>
@@ -58,7 +58,7 @@ const SlickSlider =()=> {
                   <h3 className="pt-0 pt-sm-3 text-right text-white pr-3 erbaum-bold">02</h3>
                   <div className="card-data text-white position-absolute p-3">
                     <h4 className="erbaum-bold pr-0 pr-sm-0 pr-lg-2 pf-xl-5 text-uppercase" style={{fontSize:'18px'}}>PROPPER BACK SUPPORT AND ALIGNMENT</h4>
-                    <p className="proxima-eb text-1" style={{letterSpacing:'1px'}}>The majority of people go by their daily life with improper posture while sitting, standing, or lifting heavy objects. This improper posture puts unnecessary strain on your back which leads to back pain and potentially more serious health problems in the future.</p>
+                    <p className="proxima-r text-1" style={{letterSpacing:'1px'}}>The majority of people go by their daily life with improper posture while sitting, standing, or lifting heavy objects. This improper posture puts unnecessary strain on your back which leads to back pain and potentially more serious health problems in the future.</p>
                   </div>
                 </div>
               </div>
@@ -69,7 +69,7 @@ const SlickSlider =()=> {
                   <h3 className="pt-0 pt-sm-3 text-right text-white pr-3 erbaum-bold">03</h3>
                   <div className="card-data text-white position-absolute p-3">
                     <h4 className="erbaum-bold pr-0 pr-sm-0 pr-lg-2 pf-xl-5 text-uppercase" style={{fontSize:'18px'}}>BALANCED SLEEP TEMPERATURE</h4>
-                    <p className="proxima-eb text-1" style={{letterSpacing:'1px'}}>Sleeping at night when the body is too hot or too cold causes many people to wake up during the night and break their sleep cycle. It may be hard to fall back asleep when your body is uncomfortable which takes away precious hours of sleep during the night making you feel groggy and tired in the morning.</p>
+                    <p className="proxima-r text-1" style={{letterSpacing:'1px'}}>Sleeping at night when the body is too hot or too cold causes many people to wake up during the night and break their sleep cycle. It may be hard to fall back asleep when your body is uncomfortable which takes away precious hours of sleep during the night making you feel groggy and tired in the morning.</p>
                   </div>
                 </div>
               </div>
@@ -80,7 +80,7 @@ const SlickSlider =()=> {
                   <h3 className="pt-0 pt-sm-3 text-right text-white pr-3 erbaum-bold">04</h3>
                   <div className="card-data text-white position-absolute p-3">
                     <h4 className="erbaum-bold pr-0 pr-sm-0 pr-lg-2 pf-xl-5 text-uppercase" style={{fontSize:'18px'}}>Eliminates motion transfer </h4>
-                    <p className="proxima-eb text-1" style={{letterSpacing:'1px'}}>Partner movement while you sleep can be felt across a mattress with traditional coils. This is caused by tossing and turning during the night and can affect the sleep pattern of the person sleeping as well as the partner they are sleeping with.</p>
+                    <p className="proxima-r text-1" style={{letterSpacing:'1px'}}>Partner movement while you sleep can be felt across a mattress with traditional coils. This is caused by tossing and turning during the night and can affect the sleep pattern of the person sleeping as well as the partner they are sleeping with.</p>
                   </div>
                 </div>
               </div>
@@ -91,7 +91,7 @@ const SlickSlider =()=> {
                   <h3 className="pt-0 pt-sm-3 text-right text-white pr-3 erbaum-bold">05</h3>
                   <div className="card-data text-white position-absolute p-3">
                     <h4 className="erbaum-bold pr-0 pr-sm-0 pr-lg-2 pf-xl-5 text-uppercase" style={{fontSize:'18px'}}>Resists sagging</h4>
-                    <p className="proxima-eb text-1" style={{letterSpacing:'1px'}}>One of the most common problems people have which causes them to replace their mattress is sagging. These dips in the mattress are caused by years of use and often lead to great discomfort and irritable sleep.</p>
+                    <p className="proxima-r text-1" style={{letterSpacing:'1px'}}>One of the most common problems people have which causes them to replace their mattress is sagging. These dips in the mattress are caused by years of use and often lead to great discomfort and irritable sleep.</p>
                   </div>
                 </div>
               </div>
